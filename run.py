764b65c27a0f3adaecc74f55b8d778d089846fdb
Shradha Kallaje <shradhakallaje@Shradhas-MacBook-Pro.local>
2023-12-31 15:15:27 +0530

    Adding function to get input for the arithmetic option from user

--- run.py
+++ run.py
@@ -1,3 +1,33 @@
 
+def get_numbers_input():
+    print("Please choose what arithmetic operation you wish to perform")
+    print("You can enter the following options: h, x, +, -, *, /")
+
+    while True:
+        option = input("Please enter an option:\n")
+
+        if validate_option(option):
+            print("valid option!")
+            break
+    return option
 
 
+def validate_option(option):
+    """
+    raises valueError if a value other than
+    the ones mentioned in the print statement is entered
+    """
+    # print(values)
+    try:
+        options = ['h', 'x', '+', '-', '*', '/']
+        if option not in options:
+            raise ValueError(
+                f"Wrong option entered! You can enter only h, x, +, -, *, /"
+            )
+    except ValueError as e:
+        print(f"Invalid option: {e}, please try again.\n")
+        return False
+
+    return True
+
+option = get_numbers_input()
